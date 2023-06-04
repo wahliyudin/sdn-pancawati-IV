c2860e82be8833e9abd8f9fb18443ac14da72b5e
wahliyudin <wahliyudinzein@gmail.com>
2023-06-04 11:42:22 +0700

    fix bug

--- resources/views/panitia/siswa/data-orang-tua.blade.php
+++ resources/views/panitia/siswa/data-orang-tua.blade.php
@@ -10,7 +10,7 @@
                     <label for="nama_ayah">Nama Ayah</label>
                     <input disabled type="text" name="nama_ayah"
                         class="form-control {{ $errors->has('nama_ayah') ? ' is-invalid' : '' }}" id="nama_ayah"
-                        value="{{ old('nama_ayah', $orang_tua->father->nama) }}" placeholder="Nama Ayah">
+                        value="{{ old('nama_ayah', $orang_tua?->father?->nama) }}" placeholder="Nama Ayah">
                     @error('nama_ayah')
                         <p class="invalid-feedback">{{ $message }}</p>
                     @enderror
@@ -20,7 +20,7 @@
                     <input disabled type="text"
                         class="form-control {{ $errors->has('tempat_lahir_ayah') ? ' is-invalid' : '' }}"
                         id="tempat_lahir_ayah" name="tempat_lahir_ayah"
-                        value="{{ old('tempat_lahir_ayah', $orang_tua->father->tempat_lahir) }}"
+                        value="{{ old('tempat_lahir_ayah', $orang_tua?->father?->tempat_lahir) }}"
                         placeholder="Tempat Lahir Ayah">
                     @error('tempat_lahir_ayah')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -30,7 +30,7 @@
                     <label for="email_ayah">E-Mail ayah</label>
                     <input disabled type="email"
                         class="form-control {{ $errors->has('email_ayah') ? ' is-invalid' : '' }}" id="email_ayah"
-                        name="email_ayah" value="{{ old('email_ayah', $orang_tua->father->email) }}"
+                        name="email_ayah" value="{{ old('email_ayah', $orang_tua?->father?->email) }}"
                         placeholder="E-Mail ayah">
                     @error('email_ayah')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -43,7 +43,7 @@
                     <input disabled type="text"
                         class="form-control {{ $errors->has('pekerjaan_ayah') ? ' is-invalid' : '' }}"
                         id="pekerjaan_ayah" name="pekerjaan_ayah"
-                        value="{{ old('pekerjaan_ayah', $orang_tua->father->pekerjaan) }}"
+                        value="{{ old('pekerjaan_ayah', $orang_tua?->father?->pekerjaan) }}"
                         placeholder="Pekerjaan Ayah">
                     @error('pekerjaan_ayah')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -55,7 +55,7 @@
                         <input disabled type="text" name="tanggal_lahir_ayah"
                             class="form-control datetimepicker-input {{ $errors->has('tanggal_lahir_ayah') ? ' is-invalid' : '' }}"
                             data-target="#tanggal_lahir_ayah"
-                            value="{{ old('tanggal_lahir_ayah', $orang_tua->father->tanggal_lahir) }}">
+                            value="{{ old('tanggal_lahir_ayah', $orang_tua?->father?->tanggal_lahir) }}">
                         <div class="input-group-append" data-target="#tanggal_lahir_ayah" data-toggle="datetimepicker">
                             <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                         </div>
@@ -68,7 +68,7 @@
                     <label for="no_hp_ayah">No.Telp ayah</label>
                     <input disabled type="number"
                         class="form-control {{ $errors->has('no_hp_ayah') ? ' is-invalid' : '' }}" id="no_hp_ayah"
-                        name="no_hp_ayah" value="{{ old('no_hp_ayah', $orang_tua->father->no_hp) }}"
+                        name="no_hp_ayah" value="{{ old('no_hp_ayah', $orang_tua?->father?->no_hp) }}"
                         placeholder="No.Telp ayah">
                     @error('no_hp_ayah')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -84,7 +84,7 @@
                     <label for="nama_ibu">Nama ibu</label>
                     <input disabled type="text"
                         class="form-control {{ $errors->has('nama_ibu') ? ' is-invalid' : '' }}" id="nama_ibu"
-                        name="nama_ibu" value="{{ old('nama_ibu', $orang_tua->mother->nama) }}"
+                        name="nama_ibu" value="{{ old('nama_ibu', $orang_tua->mother?->nama) }}"
                         placeholder="Nama ibu">
                     @error('nama_ibu')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -95,7 +95,7 @@
                     <input disabled type="text"
                         class="form-control {{ $errors->has('tempat_lahir_ibu') ? ' is-invalid' : '' }}"
                         id="tempat_lahir_ibu" name="tempat_lahir_ibu"
-                        value="{{ old('tempat_lahir_ibu', $orang_tua->mother->tempat_lahir) }}"
+                        value="{{ old('tempat_lahir_ibu', $orang_tua->mother?->tempat_lahir) }}"
                         placeholder="Tempat Lahir ibu">
                     @error('tempat_lahir_ibu')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -105,7 +105,7 @@
                     <label for="email_ibu">E-Mail ibu</label>
                     <input disabled type="email"
                         class="form-control {{ $errors->has('email_ibu') ? ' is-invalid' : '' }}" id="email_ibu"
-                        name="email_ibu" value="{{ old('email_ibu', $orang_tua->mother->email) }}"
+                        name="email_ibu" value="{{ old('email_ibu', $orang_tua->mother?->email) }}"
                         placeholder="E-Mail ibu">
                     @error('email_ibu')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -118,7 +118,7 @@
                     <input disabled type="text"
                         class="form-control {{ $errors->has('pekerjaan_ibu') ? ' is-invalid' : '' }}"
                         id="pekerjaan_ibu" name="pekerjaan_ibu"
-                        value="{{ old('pekerjaan_ibu', $orang_tua->mother->pekerjaan) }}"
+                        value="{{ old('pekerjaan_ibu', $orang_tua->mother?->pekerjaan) }}"
                         placeholder="Pekerjaan ibu">
                     @error('pekerjaan_ibu')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -130,8 +130,9 @@
                         <input disabled type="text" name="tanggal_lahir_ibu"
                             class="form-control datetimepicker-input {{ $errors->has('tanggal_lahir_ibu') ? ' is-invalid' : '' }}"
                             data-target="#tanggal_lahir_ibu"
-                            value="{{ old('tanggal_lahir_ibu', $orang_tua->mother->tanggal_lahir) }}">
-                        <div class="input-group-append" data-target="#tanggal_lahir_ibu" data-toggle="datetimepicker">
+                            value="{{ old('tanggal_lahir_ibu', $orang_tua->mother?->tanggal_lahir) }}">
+                        <div class="input-group-append" data-target="#tanggal_lahir_ibu"
+                            data-toggle="datetimepicker">
                             <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                         </div>
                     </div>
@@ -143,7 +144,7 @@
                     <label for="no_hp_ibu">No.Telp ibu</label>
                     <input disabled type="number"
                         class="form-control {{ $errors->has('no_hp_ibu') ? ' is-invalid' : '' }}" id="no_hp_ibu"
-                        name="no_hp_ibu" value="{{ old('no_hp_ibu', $orang_tua->mother->no_hp) }}"
+                        name="no_hp_ibu" value="{{ old('no_hp_ibu', $orang_tua->mother?->no_hp) }}"
                         placeholder="No.Telp ibu">
                     @error('no_hp_ibu')
                         <p class="invalid-feedback">{{ $message }}</p>
@@ -154,8 +155,7 @@
                 <div class="form-group">
                     <label for="pekerjaan_ibu">Alamat</label>
                     <textarea disabled name="alamat" id="" cols="30"
-                        class="form-control {{ $errors->has('alamat') ? ' is-invalid' : '' }}"
-                        placeholder="Alamat">{{ old('alamat', $orang_tua->alamat) }}</textarea>
+                        class="form-control {{ $errors->has('alamat') ? ' is-invalid' : '' }}" placeholder="Alamat">{{ old('alamat', $orang_tua->alamat) }}</textarea>
                     @error('alamat')
                         <p class="invalid-feedback">{{ $message }}</p>
                     @enderror
